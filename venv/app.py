f15c2052e72ff02910e090654936f1b047aa9caf
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2026-01-12 15:38:37 +0100

    chore: floats "

---
+++ venv/app.py
@@ -0,0 +1,16 @@
+def main():
+    print("Hello from my Python app!")
+
+if __name__ == "__main__":
+    main()
+
+# variables
+
+age = 21 # integer
+print("You're " + str(age) + " years old") # concatenation method
+print(f"You're {age} years old")  # f-string method
+
+# float
+height = 5.9 # float
+print(f"Your height is {height} feet")  # f-string method
+
