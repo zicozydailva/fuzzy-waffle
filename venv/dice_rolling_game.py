b18e4cf424b337e19d432d3686c446ec8df6dc42
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2026-01-19 10:13:32 +0100

    feat: dice rolling game complete func

--- venv/dice_rolling_game.py
+++ venv/dice_rolling_game.py
@@ -9,9 +9,15 @@
 
 import random
 
-choice = input('Roll the dice? (y/n): ').lower()
-if choice == 'y':
-    die1 = random.randint(1, 6)
-    die2 = random.randint(1, 6)
-    total = die1 + die2
-    print(f'You rolled a {die1} and a {die2}. Total: {total}')
+while True:
+    choice = input('Roll the dice? (y/n): ').lower()
+    if choice == 'y':
+        die1 = random.randint(1, 6)
+        die2 = random.randint(1, 6)
+        total = die1 + die2
+        print(f'You rolled a {die1} and a {die2}. Total: {total}')
+    elif choice == 'n':
+        print('Game exited')
+        break
+    else:
+        print('Invalid input!')
